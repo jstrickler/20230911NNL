f25b3dbc76f82218b812ee1666e9bac431bc039a
jstrickler <jstrickler@gmail.com>
2023-09-11 15:06:27 -0400

    Monday 3:30 PM

---
+++ if_statement.py
@@ -0,0 +1,45 @@
+
+value = 56
+
+if value > 75:
+    print("wombat")
+    print("wallaby")
+elif value > 50:
+    print("platypus")
+    print("quokka")
+else:
+    print("kangaroo")
+    print("koala")
+
+print("** ALL DONE **")
+
+# True
+#   numeric non-0
+#   len(x) > 0
+#   everything else that's not false :-)
+
+# False
+#    numeric 0
+#    len(x) == 0
+#    None
+
+print(True, False)
+
+x = 99
+if x:   #   if bool(x) is True
+    print("pastafazool")
+
+
+#  == != > < >= <= is
+
+#  not and or
+
+
+
+
+
+
+
+
+
+
